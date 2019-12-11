6490dd58b2421fb85bbf6e283a6b2a37fb0875fc
Eskelinen Meri <merituulie@hotmail.fi>
2019-12-11 11:35:55 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Carlist from './components/Carlist';
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/ToolBar';
+import Typography from '@material-ui/core/Typography';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="static">
+        <ToolBar>
+          <Typography variant="h6" >
+            Carshop
+          </Typography>
+        </ToolBar>
+      </AppBar>
+      <Carlist />
     </div>
   );
 }
